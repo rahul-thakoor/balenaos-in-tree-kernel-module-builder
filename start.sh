f5233f86f7703a3fd8412388445a958f8d7ba6be
Rahul Thakoor <rahul.thakoor@gmail.com>
2023-06-16 18:05:25 +0400

    load module cw2015_battery

--- start.sh
+++ start.sh
@@ -3,6 +3,8 @@ set -x
 
 echo "starting ..."
 
+modinfo /in-tree-kernel-modules/lib/modules/`uname -r`/kernel/drivers/power/supply/cw2015_battery.ko.xz
 
+insmod /in-tree-kernel-modules/lib/modules/`uname -r`/kernel/drivers/power/supply/cw2015_battery.ko.xz
 
 sleep infinity
